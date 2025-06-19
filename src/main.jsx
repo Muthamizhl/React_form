c88948c96170f798d7755e20247a7742590df76a
muthamizhraja5@gmail.com <youremail@example.com>
2025-06-19 19:48:00 +0530

    updateeeeee

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-// main.jsx or index.jsx
+// src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <BrowserRouter basename="/React_form">  {/* 👈 this is important */}
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
